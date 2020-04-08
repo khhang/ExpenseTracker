ce666636fb5045c8eea83d44b66cc8f7efb91182
Brian <brian.qk.pham@gmail.com>
2020-04-07 19:49:31 -0700

    added service for basic API calls to get expenses and expense details

---
+++ src/app/services/transactions.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionsService {
+
+  constructor(private http: HttpClient) { }
+
+  getExpenses(): Observable<Expense[]> {
+    return this.http.get<Expense[]>(`${environment.apiUrl}/expenses`);
+  }
+
+  getExpenseById(id: number): Observable<Expense> {
+    return this.http.get<Expense>(`${environment.apiUrl}/expenses/${id}`);
+  }
+
+  createExpense(expense: Expense): Observable<Expense> {
+    return this.http.post<Expense>(`${environment.apiUrl}/expenses`, expense);
+  }
+
+  updateExpense(expense: Expense): Observable<Expense> {
+    return this.http.put<Expense>(`${environment.apiUrl}/expenses/${expense.id}`, expense);
+  }
+
+  deleteExpense(id: number): Observable<any> {
+    return this.http.delete<any>(`${environment.apiUrl}/expenses/${id}`);
+  }
+
+  getExpenseDetails(): Observable<ExpenseDetail[]> {
+    return this.http.get<ExpenseDetail[]>(`${environment.apiUrl}/expenses/details`);
+  }
+
+  getExpenseDetailById(id: number): Observable<ExpenseDetail> {
+    return this.http.get<ExpenseDetail>(`${environment.apiUrl}/expenses/details/${id}`);
+  }
+}
+
+export interface Expense {
+  id?: number;
+  description: string;
+  amount: number;
+  expenseCategoryId: number;
+  expenseSubcategoryId?: number;
+  accountId: number;
+  createDate: string;
+  modifyDate: string;
+}
+
+export interface ExpenseDetail extends Expense {
+  accountName: string;
+  expenseCategoryName: string;
+  expenseSubcategoryName: string;
+}

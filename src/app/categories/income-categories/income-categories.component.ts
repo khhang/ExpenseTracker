8f0854137ff3971a667a883c53fe5d5eb8e1fb9c
Brian <brian.qk.pham@gmail.com>
2020-04-08 22:18:41 -0700

    Added handler for deleting and editing income categories

--- src/app/categories/income-categories/income-categories.component.ts
+++ src/app/categories/income-categories/income-categories.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { IncomeCategory, CategoriesService } from 'src/app/services/categories.service';
+import { IncomeCategory, CategoriesService, ExpenseCategory } from 'src/app/services/categories.service';
 import { ModalController, AlertController } from '@ionic/angular';
 import { ToastService } from 'src/app/services/toast.service';
 import { Observable } from 'rxjs';
+import { EditCategoryModalComponent } from '../edit-category-modal/edit-category-modal.component';
 
 @Component({
   selector: 'app-income-categories',
@@ -21,5 +22,48 @@ export class IncomeCategoriesComponent implements OnInit {
   ) { }
 
   ngOnInit() {}
+
+  async openEditCategoryModal(category: ExpenseCategory): Promise<void> {
+    const modal = await this.modalController.create({
+      component: EditCategoryModalComponent,
+      componentProps: category
+    });
+
+    await modal.present();
+
+    await modal.onWillDismiss()
+      .then(result => {
+        if (result.data) {
+          this.categoriesService.updateIncomeCategory(result.data).subscribe(() => {
+            this.categoriesService.refresh$.next(undefined);
+            this.toastService.presentToast('Edited the income category');
+          });
+        }
+      });
+  }
+
+  async openDeleteCategoryConfirmation(cat: ExpenseCategory): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Delete Income Category',
+      message: `Are you sure you want to delete the income category ${cat.name}?`,
+      buttons: [
+        {
+          text: 'Yes',
+          handler: () => {
+            this.categoriesService.deleteIncomeCategory(cat.id).subscribe(() => {
+              this.categoriesService.refresh$.next(undefined);
+              this.toastService.presentToast('Deleted the income category');
+            });
+          }
+        },
+        {
+          text: 'Cancel',
+          cssClass: 'secondary'
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
